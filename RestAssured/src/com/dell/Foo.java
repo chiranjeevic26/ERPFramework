e6c1823f10eafb349e74b85f18b93c146dcff94a
Chiranjeevi <chiranjeevi_ch@outlook.com>
2020-07-22 02:33:56 +0530

    full code commit

---
+++ RestAssured/src/com/dell/Foo.java
@@ -0,0 +1,20 @@
+package com.dell;
+
+public class Foo {
+	public static void main(String[] args) {
+		/*
+		 * boolean a = false; boolean b=true; if(a) { System.out.println("A"); }else
+		 * if(a&&b) { System.out.println("A&&B"); }else { if(!b) {
+		 * System.out.println("not B"); }else { System.out.println("ELSE"); } }
+		 */
+		System.out.println(10+20+"JavaTpoin");
+		System.out.println("javaTpoint"+10+20);
+	}
+	
+
+	public static void tm(boolean a, boolean b) 
+	{
+		
+	}
+
+}
